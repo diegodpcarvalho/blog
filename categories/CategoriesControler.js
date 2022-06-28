ab0848609dd69adbfde44ba2a4ea97b6be82beeb
diegodpcarvalho <diegodpcarvalho@gmail.com>
2022-06-27 23:54:47 -0300

    formulario de edição

--- categories/CategoriesControler.js
+++ categories/CategoriesControler.js
@@ -28,5 +28,44 @@ router.get("/admin/categories", (req, res) =>{
     })
 })
 
+router.post("/categories/delete",(req, res) =>{
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id:id
+                }
+        }).then(()=>{
+            res.redirect("/admin/categories")
+        });
+            }else{
+                res.redirect("/admin/categories");
+            }
+    }else{
+            res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res)=>{
+    let id = req.params.id;
+
+    id(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category =>{
+        
+        if(category != undefined){
+
+            res.render("admin/categories/edit",{category: category});
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro=>{
+        res.redirect("/admin/categories");
+    })
+})
 
 module.exports = router;
