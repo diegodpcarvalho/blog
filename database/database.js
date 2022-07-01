f354b4792d07845b27b0dd5767d39108353d5fdf
diegodpcarvalho <diegodpcarvalho@gmail.com>
2022-07-01 18:25:51 -0300

    salvando artigos

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require ("sequelize")
 
 const connection = new Sequelize('guiapress','root','Reanimation',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
