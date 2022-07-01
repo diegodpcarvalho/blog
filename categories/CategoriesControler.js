7926430bfec881d66b2c4cfd7b9e991cacf9d7f7
diegodpcarvalho <diegodpcarvalho@gmail.com>
2022-06-30 23:26:45 -0300

    dropdown de categorias

--- categories/CategoriesControler.js
+++ categories/CategoriesControler.js
@@ -15,7 +15,7 @@ router.post("/categories/save",(req, res)=>{
             title: title,
             slug:slugify(title)
         }).then(() =>{
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     }else{
         res.redirect("/admin/categories/new");
@@ -50,9 +50,10 @@ router.post("/categories/delete",(req, res) =>{
 router.get("/admin/categories/edit/:id", (req, res)=>{
     let id = req.params.id;
 
-    id(isNaN(id)){
+    if(isNaN(id)){
         res.redirect("/admin/categories");
     }
+    
 
     Category.findByPk(id).then(category =>{
         
@@ -64,6 +65,19 @@ router.get("/admin/categories/edit/:id", (req, res)=>{
             res.redirect("/admin/categories");
         }
     }).catch(erro=>{
+        res.redirect("/admin/categories");
+    })
+});
+
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(()=>{
         res.redirect("/admin/categories");
     })
 })
